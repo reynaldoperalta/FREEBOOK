abcec3b48880fcfd8f014fb94532278f1a773c1b
reynaldoperalta <reynaldopema14@gmail.com>
2022-06-28 00:48:06 -0500

    Add config db

--- config/conexion.js
+++ config/conexion.js
@@ -2,11 +2,12 @@
 var mysql = require("mysql");
 var conexion = mysql.createConnection({
 
-    host:'localhost',
-    user:'reynaldo',
-    password:'reynaldo14',
-    database:'proyectolibros'
+    host:'us-cdbr-east-05.cleardb.net',
+    user:'b0ca7c644054c7',
+    password:'4e13a6c4',
+    database:'heroku_0b9bb8f208c4153'
 });
+//mysql://b0ca7c644054c7:4e13a6c4@us-cdbr-east-05.cleardb.net/heroku_0b9bb8f208c4153?reconnect=true
 
 //Mensaje de error:
 conexion.connect(
